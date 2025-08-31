2c8950414d453db51da62ebc19627cf33db1427c
Magic-Dog <magicdog@example.com>
2025-09-01 06:18:03 +0800

    Update MagicDog Linux deploy package - 2025-09-01 06:18:03

---
+++ magicdog_linux_deploy/AdminIndex/assets/auth-BOCkWHUz.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-DatkZRbF.js";function o(){const t=e();function n(u){return t.isLogin?typeof u=="string"?t.userInfo.buttons.includes(u):u.some(r=>t.userInfo.buttons.includes(r)):!1}return{hasAuth:n}}export{o as u};
